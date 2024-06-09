7c4e1b456d6259b6032d3fbdc6610956be4f46a1
Bhavanar24 <161867667+Bhavanar24@users.noreply.github.com>
2024-06-09 15:10:45 +0530

    Delete server directory

--- server/src/routes/image.route.ts
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { uploadImage } from "../controllers/image.controller";
-
-const imageRouter = express.Router();
-
-imageRouter.post("/upload", uploadImage);
-
-export default imageRouter;
