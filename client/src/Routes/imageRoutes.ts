e9206ddb5a3e9ea2b3711bcc1a31da0f298fbe69
Bhavanar24 <161867667+Bhavanar24@users.noreply.github.com>
2024-06-09 15:10:28 +0530

    Delete client directory

--- client/src/Routes/imageRoutes.ts
+++
@@ -1,3 +0,0 @@
-export const imageRoutes = {
-    upload: `${process.env.REACT_APP_BACKEND_APP_BASE_URL}/image/upload`,
-};
--- client/src/Routes/notificationRoutes.ts
+++
@@ -1,8 +0,0 @@
-export const notificationRoutes = {
-    getUserNotifications: `${process.env.REACT_APP_BACKEND_APP_BASE_URL}/notification/user-notifications`,
-    markAsReadNotification:
-      `${process.env.REACT_APP_BACKEND_APP_BASE_URL}/notification/mark-as-read`,
-    markAllAsReadNotifications: `${process.env.REACT_APP_BACKEND_APP_BASE_URL}/notification/mark-all-as-read`,
-    deleteNotification: `${process.env.REACT_APP_BACKEND_APP_BASE_URL}/notification/delete-notifications`
-  };
-  
